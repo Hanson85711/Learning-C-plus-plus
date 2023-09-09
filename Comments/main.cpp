1a6d8154fedc2b80246acacc326756485a61d7b4
Hanson Lee <icycandice@gmail.com>
2023-09-08 18:48:52 -0700

    Comments
    Example of the two ways of commenting

---
+++ Comments/main.cpp
@@ -0,0 +1,7 @@
+//Can write comments with two forward slashes
+//Only works on the line the slashes are on
+
+/*
+Or type /* and the idea will generate the end
+and everything in between is considered a comment like this
+*/
