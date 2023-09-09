586556b6fa28eada40f149f17a18b196fc8b295b
Hanson Lee <icycandice@gmail.com>
2023-09-08 17:41:26 -0700

    Naming Conventions

---
+++ Naming Conventions/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main () {
+    int file_size; //Snake Case
+    int FileSize; //Pascal Case
+    int fileSize; //Camel Case
+    int iFileSize; //Hungarian Notation Note: Old notation. Start with lowercase letter of variable type before following with Pascal Case
+    return 0; 
+}
