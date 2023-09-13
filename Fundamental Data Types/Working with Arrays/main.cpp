e7539e80be5c3770f0630dbea89c1022cf98bed2
Hanson Lee <icycandice@gmail.com>
2023-09-12 21:51:46 -0700

    Working with Arrays

---
+++ Fundamental Data Types/Working with Arrays/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+#include <string>
+
+
+using namespace std; 
+
+/*Exercise
+Ask the user for 3 names. Store these names in an array and print 
+the first name*/
+
+int threeNames() {
+
+  string names[3]; 
+  
+  cout<<"Gimme 3 names: ";
+  getline(cin,names[0]);
+  getline(cin,names[1]);
+  getline(cin,names[2]);
+  
+
+
+  cout<<names[0]<<endl;
+
+  return 0;
+}
+
+int main() {
+  threeNames();
+}
